26715f535741c371136b8872f1d4bcf8a6343833
JulianPoh <julianpoh01@gmail.com>
2018-08-16 22:54:49 +0800

    final changes

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,9 @@ class Post < ApplicationRecord
     belongs_to :user
     acts_as_commontable dependent: :destroy
     attr_accessor :image
+    scope :creatures, -> { where(category: 'Creatures') }
+    scope :objects, -> { where(category: 'Objects') }
+    scope :medical, -> { where(category: 'Medical') }
 
     mount_uploader :image, ImageUploader
 end
